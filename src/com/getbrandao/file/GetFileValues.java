44f36162ef44e1f93a380c17f3b33eab88b0f236
Gustavo Brandao <gustavo.brandao@bsd.com.br>
2016-03-11 09:28:22 -0300

    Deleting some lines

--- src/com/getbrandao/file/GetFileValues.java
+++ src/com/getbrandao/file/GetFileValues.java
@@ -12,11 +12,6 @@ public class GetFileValues {
 	InputStream is;
 	Properties prop = new Properties();
 	String propFileName = "config.properties";
-//	String dir;
-//	String name;
-//	String client;
-//	String server;
-//	String port;
 	
 	public Map<String,String> getPropValues() throws IOException {
 		
